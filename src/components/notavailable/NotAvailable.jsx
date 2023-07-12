3b50af3248daed5dabdb409c7d9f11a1db2d93da
Aditya Bhatt <adityabhatt789@gmail.com>
2023-07-12 16:28:02 +0530

    Completed

---
+++ src/components/notavailable/NotAvailable.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotAvailable = () => {
+  return (
+    <div className='container'>
+    <div className="title">
+    Feature not Available
+    </div>
+    <button>Go to Home</button>
+    </div>
+  )
+}
+
+export default NotAvailable
